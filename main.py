1cc50ec58d6fc65d4a2d7c1b6a7923bcf993956a
littleplus <littleplus@users.noreply.github.com>
2017-06-23 12:50:07 +0800

    Update main.py
    Create file by date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #encoding:utf-8
-import re,requests,sys
+import re,requests,sys,time
 from bs4 import BeautifulSoup
 
 reload(sys)
@@ -15,12 +15,14 @@ soup=BeautifulSoup(page.text)
 
 td=soup.find_all('td')
 
+date=time.strftime('%Y-%m-%d',time.localtime(time.time()))
+
 for id,x in enumerate(td):
 	pat=re.compile(r"(\d+)\.(\d+)\.(\d+)\.(\d+)")
 	if(pat.match(x.text)):
 		try:
 			if(requests.get(url=url2,proxies={"https":"http://%s:%s" % (x.text,td[id+1].text)},timeout=3).status_code==200):
-        f=open('/root/ip.txt','a')
+				f=open('/root/ip_%s.txt'%(date),'a')
 				f.write(x.text+'\t'+td[id+1].text+'\t'+td[id+2].text+'\n')
 				f.close()
 		except:
